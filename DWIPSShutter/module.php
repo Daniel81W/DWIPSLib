9cec72b1d60b35b11ea7e1435408f44a4aa5e5c2
Daniel81W <kenya81@gmail.com>
2021-03-11 16:48:11 +0100

    Shutter angelegt

---
+++ DWIPSShutter/module.php
@@ -0,0 +1,38 @@
+<?php
+
+	//include_once("/var/lib/symcon/modules/DWIPSLib/libs/astro.php");
+	class DWIPSShutter extends IPSModule {
+
+		public function Create()
+		{
+			//Never delete this line!
+			parent::Create();
+			
+		}
+
+		public function Destroy()
+		{
+			//Never delete this line!
+			parent::Destroy();
+		}
+
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+			
+		}
+
+		/**
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+        *
+        * DWIPSShutter_UpdateSunrise($id);
+        *
+        */
+        public function UpdateSunrise() {
+           }
+
+		
+	}
+	?>

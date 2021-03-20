2e640e81eb9e25b8b10d4883c606a9685b108eea
Daniel81W <kenya81@gmail.com>
2021-03-20 10:22:41 +0100

    Lichtmodul erzeugt

---
+++ DWIPSLight/module.php
@@ -0,0 +1,39 @@
+<?php
+
+	//include_once("/var/lib/symcon/modules/DWIPSLib/libs/astro.php");
+	class DWIPSLight extends IPSModule {
+
+		public function Create()
+		{
+			//Never delete this line!
+			parent::Create();
+			echo $this->InstanceID;
+			
+		}
+
+		public function Destroy()
+		{
+			//Never delete this line!
+			parent::Destroy();
+		}
+
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+			
+		}
+
+		/**
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+        *
+        * DWIPSShutter_UpdateSunrise($id);
+        *
+        */
+        public function UpdateSunrise() {
+           }
+
+		
+	}
+	?>

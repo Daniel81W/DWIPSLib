08dab0dc0791b733f7bd93f11bee0879e16e09e0
Daniel81W <kenya81@gmail.com>
2021-07-09 12:58:45 +0200

    upd

--- DWIPSShutter/module.php
+++ DWIPSShutter/module.php
@@ -98,7 +98,7 @@
 					//Neuen Wert in die Statusvariable schreiben
 					SetValue($this->GetIDForIdent($Ident), $Value);
 					//KNX oder EIB
-					echo IPS_GetInstance($this->ReadPropertyInteger("PositionInstanceID"));
+					print_r(IPS_GetInstance($this->ReadPropertyInteger("PositionInstanceID")));
 					//echo $test[ModuleInfo][ModuleID];
 					KNX_WriteDPT5($this->ReadPropertyInteger("PositionInstanceID"), $Value);
 					break;

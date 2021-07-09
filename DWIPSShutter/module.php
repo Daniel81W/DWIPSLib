231353c8762c202ec4e3a822c28b9e04aeba3c8b
Daniel81W <kenya81@gmail.com>
2021-07-09 18:02:52 +0200

    upd

--- DWIPSShutter/module.php
+++ DWIPSShutter/module.php
@@ -75,11 +75,7 @@
 			$PosScriptID = IPS_CreateScript(0);
 			IPS_SetParent($PosScriptID, IPS_GetChildrenIDs($this->ReadPropertyInteger("PositionInstanceID"))[0]);
 			IPS_SetName($PosScriptID, "DWIPS_ActionScript");
-			$cont = <<<EOS
-				<? SetValue($_IPS['VARIABLE'], $_IPS['VALUE']);DWIPSShutter_UpdatePositionValue( $this->InstanceID , $_IPS['VALUE']); ?>
-			EOS;
-			echo $cont;
-			/*IPS_SetScriptContent($PosScriptID, "<? SetValue($_IPS['VARIABLE'], $_IPS['VALUE']);DWIPSShutter_UpdatePositionValue(".$this->InstanceID.", $_IPS['VALUE']); ?>");*/
+			IPS_SetScriptContent($PosScriptID, "<? SetValue(");//$_IPS['VARIABLE'], $_IPS['VALUE']);DWIPSShutter_UpdatePositionValue(".$this->InstanceID.", $_IPS['VALUE']); ?>");*/
 			IPS_SetVariableCustomAction(IPS_GetChildrenIDs($this->ReadPropertyInteger("PositionInstanceID"))[0], $PosScriptID);
 			
 			

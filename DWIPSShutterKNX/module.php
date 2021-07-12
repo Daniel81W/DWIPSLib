18dd45d53f6dd434ea478a94dddeed10ac7091d4
Daniel81W <kenya81@gmail.com>
2021-07-12 10:29:24 +0200

    upd

--- DWIPSShutterKNX/module.php
+++ DWIPSShutterKNX/module.php
@@ -129,7 +129,7 @@
 
 		public function ReceiveData($JSONString) {
 			$knxdata = json_decode($JSONString, true);
-			$this->SendDebug("KNX", $JSONString, 0);
+			$this->SendDebug("KNX", bin2hex($knxdata["Data"]), 0);
 //			if($knxdata["DataID"] == "{8A4D3B17-F8D7-4905-877F-9E69CEC3D579}"){
 /*			if($knxdata["GroupAddress1"] == $this->ReadPropertyInteger("Hauptgruppe") and $knxdata["GroupAddress2"] == $this->ReadPropertyInteger("Mittelgruppe") and $knxdata["GroupAddress3"] == $this->ReadPropertyInteger("Untergruppe")){
 				SetValue("Test", $knxdata["DataID"]. " - " . $knxdata["Data"]);

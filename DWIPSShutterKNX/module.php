d648c7cde5f223c0b04935aa33a1e69046ebb959
Daniel81W <kenya81@gmail.com>
2021-07-14 12:05:19 +0200

    upd

--- DWIPSShutterKNX/module.php
+++ DWIPSShutterKNX/module.php
@@ -18,11 +18,11 @@
 			$this->ConnectParent($this->parentID);
 		
 			foreach($fields as $field){
-				$this->RegisterPropertyInteger($field["name"]."MainGroup", 0);
-				$this->RegisterPropertyInteger($field["name"]."MiddleGroup", 0);
-				$this->RegisterPropertyInteger($field["name"]."SubGroup", 0);
-				$this->RegisterPropertyInteger($field["name"]."DataPointType", 1);
-				$this->RegisterPropertyInteger($field["name"]."DataPointSubType", 1);
+				$this->RegisterPropertyInteger($this->field["name"]."MainGroup", 0);
+				$this->RegisterPropertyInteger($this->field["name"]."MiddleGroup", 0);
+				$this->RegisterPropertyInteger($this->field["name"]."SubGroup", 0);
+				$this->RegisterPropertyInteger($this->field["name"]."DataPointType", 1);
+				$this->RegisterPropertyInteger($this->field["name"]."DataPointSubType", 1);
 			}
 			//Instances for control of the shutter (KNX, EIB)
 //			$this->RegisterPropertyInteger("UpDownMainGroup", 0);
@@ -376,7 +376,7 @@
 			{ "value": 10, "caption": "DPT 200"}"';
 
 			$ret = '{"elements": [';
-			foreach($this->)
+			//foreach($this->)
 
 			return $ret;
 		}

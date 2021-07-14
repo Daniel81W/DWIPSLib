59aa9a40bee7f1dca8c49561e6a71fef3d44e59e
Daniel81W <kenya81@gmail.com>
2021-07-14 17:54:53 +0200

    upd

--- DWIPSShutterKNX/module.php
+++ DWIPSShutterKNX/module.php
@@ -130,7 +130,7 @@
           			$result = intval( round( $Val[ 1 ] / 255 * 100 ) );
 					//$this->SendDebug("KNX", sizeof($Val), 0);
 					//$this->SendDebug("KNX", $Val[1], 0);
-					$this->SendDebug("KNX", $hexval, 0);
+					$this->SendDebug("KNX", $decval, 0);
 					//$this->SendDebug("KNX", bin2hex(pack( "CC", 0x80, 200 )), 0);
 
 					SetValueInteger($this->GetIDForIdent($this->Translate("Position")), hexdec($hexval));

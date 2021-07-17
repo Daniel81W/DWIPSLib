09a38435d63637a608f78387d9f04a90c821c8f1
Daniel81W <kenya81@gmail.com>
2021-07-17 11:25:25 +0200

    upd

--- DWIPSShutterKNX/module.php
+++ DWIPSShutterKNX/module.php
@@ -172,7 +172,7 @@
 		public function DecodeDPT5($data){
 			$val = bin2hex($data);
 			$val = hexdec( $val) - hexdec("c280");
-			$this->SendDebug("KNX", "hex: " . bin2hex($data) . "   -   dec: " . $val);
+			$this->SendDebug("KNX", "hex: " . bin2hex($data) . "   -   dec: " . $val, 0);
 			return $val;
 		}
 		public function EncodeDPT5($data){

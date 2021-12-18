825167c358d425e534adaa3c763cfc01d276f057
Daniel81W <kenya81@gmail.com>
2021-12-18 18:48:56 +0100

    upd

--- DWIPSShutterKNX/module.php
+++ DWIPSShutterKNX/module.php
@@ -107,7 +107,6 @@
 				}
 			}
 
-			$this->upDownDPT = new DPT1($this->ReadPropertyInteger("UpDownMainGroup"), $this->ReadPropertyInteger("UpDownMiddleGroup"), $this->ReadPropertyInteger("UpDownSubGroup"));
 		}
 
 		public function Destroy()
@@ -123,7 +122,10 @@
 			//$this->TestForm();
 
 		}
-
+		public function __construct (string $InstanzID){
+			parent::__construct ($InstanzID);
+			$this->upDownDPT = new DPT1($this->ReadPropertyInteger("UpDownMainGroup"), $this->ReadPropertyInteger("UpDownMiddleGroup"), $this->ReadPropertyInteger("UpDownSubGroup"));
+		}
 		public function ReceiveData($JSONString) {
 			$knxdata = json_decode($JSONString, true);
 			if($knxdata["DataID"] == "{8A4D3B17-F8D7-4905-877F-9E69CEC3D579}"){

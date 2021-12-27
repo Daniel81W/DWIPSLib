960cb007aac172d673b1c2638632e3922a3f69ee
Daniel81W <kenya81@gmail.com>
2021-12-27 12:45:52 +0100

    upd

---
+++ DWIPSKNXGateway/module.php
@@ -0,0 +1,50 @@
+<?php
+
+	include_once("/var/lib/symcon/modules/DWIPSLib/libs/knx.php");
+	
+	class DWIPSShutterKNX extends IPSModule {
+
+		private $parentID = "{6DC3D946-0D31-450F-A8C6-C42DB8D7D4F1}";
+
+		public function Create()
+		{
+			//Never delete this line!
+			parent::Create();
+
+			//Connect to EIBGateway
+			$this->ConnectParent($this->parentID);
+		
+
+		}
+
+		public function Destroy()
+		{
+			//Never delete this line!
+			parent::Destroy();
+		}
+
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+
+		}
+	
+		public function ReceiveData($JSONString) {
+		}
+
+		/**
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+        *
+        * DWIPSShutter_UpdatePositionValue($id);
+        *
+        */
+
+		public function RequestAction($Ident, $Value) {
+ 
+		 
+		}
+	}
+
+?>

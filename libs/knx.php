fc1c087e0ca670611c0830e2aaa88dbf75771162
Daniel81W <kenya81@gmail.com>
2022-01-08 20:07:30 +0100

    upd

--- libs/knx.php
+++ libs/knx.php
@@ -112,9 +112,10 @@
 
         public function decode($data){
 			$val = bin2hex($data);
-            if(! is_null($this->mod)){
-                
             WFC_SendPopup(47530, "KNX", "test");
+            if(! is_null($this->mod)){
+                WFC_SendPopup(47530, "KNX", "test");
+                
                 $this->mod->SendDebug("KNX DPT5", $val, 0);
             }
 			$val = hexdec( substr($val,4));// * 100 / 255;

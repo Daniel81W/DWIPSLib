f1a9cf3e413e5a63a1b78f83ff5887279214870f
Daniel81W <kenya81@gmail.com>
2022-01-15 10:04:27 +0100

    upd

--- DWIPSKNXGateway/module.php
+++ DWIPSKNXGateway/module.php
@@ -30,86 +30,79 @@
 
 		}
 	
-		public function ReceiveData($JSONString) {
+		public function ReceiveData($JSONString)
+		{
 			$this->ReceiveDataFT12($JSONString);
 		}
 
-		public function ReceiveDataFT12($JSONString) {
+		public function ReceiveDataFT12($JSONString)
+		{
 			$data = json_decode($JSONString, true);
 
 			$currentdata = $this->GetBuffer("KNXData") . bin2hex($data["Buffer"]);
 
 			$this->SendDebug("SerialPort","1. New Data", 0);
 			
-			$next = strpos($currentdata, "c2");
-			while($next !== false){
-				$torep = substr($currentdata, $next, 4);
-				$currentdata = str_replace($torep, dechex(hexdec($torep) - hexdec("C200")), $currentdata);
-				$next = strpos($currentdata, "c2");
-			}
-			$next = strpos($currentdata, "c3");
-			while($next !== false){
-				$torep = substr($currentdata, $next, 4);
-				$currentdata = str_replace($torep, dechex(hexdec($torep) - hexdec("C2C0")), $currentdata);
-				$next = strpos($currentdata, "c3");
-			}
+			$currentdata = $this->correctDataForUTFCodes($currentdata);
 
 			$this->SendDebug("SerialPort","2. - " . $currentdata, 0);
 			$i = 8;
 			while($i>0){
+				$this->SendDebug("SerialPort","i = " . $i, 0);
 				$this->SendDebug("SerialPort","A - " . $currentdata, 0);
-			//Buffer beginnt mit E5 -> ACK
-			if(strpos($currentdata, "e5") == 0){
-				$currentdata = substr($currentdata,2);
-			}
-			//$this->SendDebug("SerialPort","A", 0);
-			//Reset Req
-			elseif(strpos($currentdata, "10404016") == 0){
-				$currentdata = substr($currentdata,8);
-			}
-			//$this->SendDebug("SerialPort","B", 0);
-			// Reset Ind
-			elseif(strpos($currentdata, "10C0C016") == 0){
-				$currentdata = substr($currentdata,8);
-			}
-			//$this->SendDebug("SerialPort","C", 0);
-			//Buffer beginnt mit 68****68
-			elseif(strpos($currentdata, "68") == 0){
-				$this->SendDebug("SerialPort","D", 0);
-				if(strpos(substr($currentdata, 6, 2), "68") == 0){
-					$this->SendDebug("SerialPort","E", 0);
-					if(strcmp(substr($currentdata, 2, 2), substr($currentdata, 4, 2)) == 0){	
-						$this->SendDebug("SerialPort","F", 0);
-				$framelen = hexdec(substr($currentdata, 2, 2)) * 2 + 12;
-				$this->SendDebug("SerialPort","3. Framelen: " . $framelen, 0);
-				if(strlen($currentdata) >= $framelen){
-					$frame = substr($currentdata, 0, $framelen);
-					if($this->proofChecksum($frame)){
-						$framedata = substr($frame, 10, $framelen - 14);
-						$this->SendDebug("SerialPort","4. Ganz: " . $framedata, 0);
-					}
-					$currentdata = substr($currentdata, $framelen);
-					$this->SendDebug("SerialPort","5. Ganz: " . $currentdata, 0);
+				//Buffer beginnt mit E5 -> ACK
+				if(strpos($currentdata, "e5") == 0){
+					$currentdata = substr($currentdata,2);
 				}
-			
-			}}}
-			// Buffer beginnt nicht mit 68, bedeutet es ist nicht der Anfang eines FT1.2 Frames. Es muss zuerst er nächste Anfang gefunden werden und dann alles davor gelöscht.
-			else{ 
-				//Wenn nicht der Anfang des Frames dann muss es 1668 als Übergang zwischen den Frames geben. Position davon finden.
-				$begin = strpos($currentdata,"1668");
-				// Falls 1668 gefunden wurde
-				if($begin !== false){
-					// Index plus 2, also auf 68
-					$begin += 2;
-					// ob die Stellen 7 + 8 auch 68 sind (Muss im FT1.2 Frame)
-					if(strcmp(substr($currentdata, $begin + 6, 2), "68") == 0 ){
-						// String ab der ersten 68 als neuer Buffer
-						$currentdata = substr($currentdata, $begin);
+				//$this->SendDebug("SerialPort","A", 0);
+				//Reset Req
+				elseif(strpos($currentdata, "10404016") == 0){
+					$currentdata = substr($currentdata,8);
+				}
+				//$this->SendDebug("SerialPort","B", 0);
+				// Reset Ind
+				elseif(strpos($currentdata, "10C0C016") == 0){
+					$currentdata = substr($currentdata,8);
+				}
+				//$this->SendDebug("SerialPort","C", 0);
+				//Buffer beginnt mit 68****68
+				elseif(strpos($currentdata, "68") == 0){
+					$this->SendDebug("SerialPort","D", 0);
+					if(strpos(substr($currentdata, 6, 2), "68") == 0){
+						$this->SendDebug("SerialPort","E", 0);
+						if(strcmp(substr($currentdata, 2, 2), substr($currentdata, 4, 2)) == 0){	
+							$this->SendDebug("SerialPort","F", 0);
+							$framelen = hexdec(substr($currentdata, 2, 2)) * 2 + 12;
+							$this->SendDebug("SerialPort","3. Framelen: " . $framelen, 0);
+							if(strlen($currentdata) >= $framelen){
+								$frame = substr($currentdata, 0, $framelen);
+								if($this->proofChecksum($frame)){
+									$framedata = substr($frame, 10, $framelen - 14);
+									$this->SendDebug("SerialPort","4. Ganz: " . $framedata, 0);
+								}
+								$currentdata = substr($currentdata, $framelen);
+								$this->SendDebug("SerialPort","5. Ganz: " . $currentdata, 0);
+							}
+						}
 					}
 				}
+				// Buffer beginnt nicht mit 68, bedeutet es ist nicht der Anfang eines FT1.2 Frames. Es muss zuerst er nächste Anfang gefunden werden und dann alles davor gelöscht.
+				else{ 
+					//Wenn nicht der Anfang des Frames dann muss es 1668 als Übergang zwischen den Frames geben. Position davon finden.
+					$begin = strpos($currentdata,"1668");
+					// Falls 1668 gefunden wurde
+					if($begin !== false){
+						// Index plus 2, also auf 68
+						$begin += 2;
+						// ob die Stellen 7 + 8 auch 68 sind (Muss im FT1.2 Frame)
+						if(strcmp(substr($currentdata, $begin + 6, 2), "68") == 0 ){
+							// String ab der ersten 68 als neuer Buffer
+							$currentdata = substr($currentdata, $begin);
+						}
+					}
+				}
+				$i--;
 			}
-			$i--;
-		}
 			
 			if(strlen($currentdata)>100){
 				$currentdata = "";
@@ -119,8 +112,27 @@
 			$this->SetBuffer("KNXData", $currentdata);
 		}
 
-		private function proofChecksum(string $frame) : bool{
+		private function proofChecksum(string $frame) : bool
+		{
 			return true;
+		}
+
+		private function correctDataForUTFCodes(string $frame) : string
+		{
+			$data = $frame;
+			$next = strpos($data, "c2");
+			while($next !== false){
+				$torep = substr($data, $next, 4);
+				$data = str_replace($torep, dechex(hexdec($torep) - hexdec("C200")), $data);
+				$next = strpos($data, "c2");
+			}
+			$next = strpos($data, "c3");
+			while($next !== false){
+				$torep = substr($data, $next, 4);
+				$data = str_replace($torep, dechex(hexdec($torep) - hexdec("C2C0")), $data);
+				$next = strpos($data, "c3");
+			}
+			return $data;
 		}
 
 		/**

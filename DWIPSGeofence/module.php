b1e7f86b0d6f9db20a480616025ac3a97f7a7302
Daniel81W <kenya81@gmail.com>
2023-02-12 20:25:18 +0100

    upd

--- DWIPSGeofence/module.php
+++ DWIPSGeofence/module.php
@@ -1,8 +1,6 @@
 <?php
 
-include_once __DIR__ . '/../libs/WebHookModule.php';
-
-class DWIPSGeofence extends WebHookModule {
+class DWIPSGeofence extends IPSModule {
 
 	public function Create()
 	{

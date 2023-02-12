9e843e7fd339c4a8445397ee3c1add3c54bfb3b0
Daniel81W <kenya81@gmail.com>
2023-02-12 19:48:59 +0100

    upd

--- DWIPSGeofence/module.php
+++ DWIPSGeofence/module.php
@@ -20,11 +20,19 @@
 			parent::ApplyChanges();
 		}
 
+
+		/**
+		* This function will be called by the hook control. Visibility should be protected!
+		*/
+		protected function ProcessHookData()
+		{
+			IPS_LogMessage("GeofenceOut Post", print_r($_SERVER['HOOK'], true));
+		}
 		/**
         * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
         * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
         *
-        * DWIPSShutter_UpdateSunrise($id);
+        * DWIPSGeofence_ProcessHookData($id);
         *
         */
         public function UpdateSunrise() {
